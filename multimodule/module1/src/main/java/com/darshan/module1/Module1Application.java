3c908e72b07e9b07f01915d2caf884272c68e133
Darshan.Shah <darshan.shah@stackroute.in>
2019-11-20 18:19:27 +0530

    updated dockerFile for webapp and code for multimodule/module1

--- multimodule/module1/src/main/java/com/darshan/module1/Module1Application.java
+++ multimodule/module1/src/main/java/com/darshan/module1/Module1Application.java
@@ -2,7 +2,9 @@ package com.darshan.module1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.darshan.lib","com.darshan.module1"})
 @SpringBootApplication
 public class Module1Application {
 
